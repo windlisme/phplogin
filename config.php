9df560da049cfc2bc934adea6d890904c06e6270
windlisme <vtlong1405@gmail.com>
2024-10-30 08:13:30 +0700

    ok

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+// Thông tin kết nối cơ sở dữ liệu
+$host = 'qltv-db.cpwo840s438x.us-east-1.rds.amazonaws.com';
+$db   = 'qltvdb';
+$user = 'admin';
+$pass = 'Luuthuyvotinh20';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Lỗi kết nối cơ sở dữ liệu: " . $e->getMessage();
+    exit();
+}
+?>
